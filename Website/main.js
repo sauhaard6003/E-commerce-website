26aea77cd59aa162bdcc413293db14ca9be7d47f
sauhaard6003 <78654368+sauhaard6003@users.noreply.github.com>
2023-03-06 18:01:16 +0530

    Delete Website directory

--- Website/main.js
+++
@@ -1,9 +0,0 @@
-
-function myFunction() {
-  var x = document.getElementById("main_menu");
-  if (x.style.display === "block") {
-    x.style.display = "none";
-  } else {
-    x.style.display = "block";
-  }
-}
